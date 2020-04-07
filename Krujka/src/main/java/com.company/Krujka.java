510b803d320699033c7a66326232f2acfb9ce161
Sneg <snegok369>
2020-04-07 22:02:53 +0300

    QE-1 Решить квадратное уравнение ax^2+bx+c=-7. На вход в программу подаются значения a,b,c. На выходе хотим получить решение уравнения ( в процессе )

---
+++ Krujka/src/main/java/com.company/Krujka.java
@@ -0,0 +1,13 @@
+package com.company;
+
+class Krujka {
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 2;
+        double c = 2.75;
+        double d = b * 2 - 4 * a * c;
+        System.out.print(d);
+    }
+}
+
